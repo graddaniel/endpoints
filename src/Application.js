1097983dc5ff5d0ce510ad93ece257f4968da32e
Daniel Grad <graddaniel0@gmail.com>
2023-02-08 21:12:39 +0100

    Added http-status-codes and refactored getProfile

--- src/Application.js
+++ src/Application.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { StatusCodes } = require('http-status-codes');
 
 const { sequelize } = require('./model');
 const { getProfile } = require('./middleware/getProfile');
@@ -18,12 +19,13 @@ class Application {
          * @returns contract by id
          */
         this.app.get('/contracts/:id', getProfile, async (req, res) => {
-            const { Contract } = req.app.get('models')
+            const { Contract } = req.app.get('models');
             const { id } = req.params;
+            // console.log("ID",id)
             const contract = await Contract.findOne({ where: { id } });
 
             if (!contract) {
-                return res.status(404).end();
+                return res.status(StatusCodes.NOT_FOUND).end();
             }
 
             res.json(contract);
--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,14 +1,22 @@
 
+const { StatusCodes } = require('http-status-codes');
+
+
 async function getProfile (req, res, next) {
     const { Profile } = req.app.get('models');
 
+    const profileId = req.get('profile_id');
+    if (!profileId) {
+        return res.status(StatusCodes.UNAUTHORIZED).end();
+    }
+
     const profile = await Profile.findOne({
         where: {
-            id: req.get('profile_id') || 0,
+            id: profileId,
         },
     });
     if (!profile) {
-        return res.status(401).end();
+        return res.status(StatusCodes.UNAUTHORIZED).end();
     }
 
     req.profile = profile;
