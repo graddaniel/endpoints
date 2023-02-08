db50850df89788a7d3ab924ed59821010f31fd9d
Daniel Grad <graddaniel0@gmail.com>
2023-02-08 20:37:17 +0100

    Added config

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
+const config = require('config');
+
 const app = require('./app');
 
 init();
 
 async function init() {
   try {
-    app.listen(3001, () => {
-      console.log('Express App Listening on Port 3001');
+    const port = config.get('server.port')
+
+    app.listen(port, () => {
+      console.log(`Express App Listening on Port ${port}`);
     });
+
   } catch (error) {
     console.error(`An error occurred: ${JSON.stringify(error)}`);
     process.exit(1);
