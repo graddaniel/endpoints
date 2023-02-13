60b60ebb5735536c0216ec3b1b8aa84445f06b41
Daniel Grad <graddaniel0@gmail.com>
2023-02-13 20:19:41 +0100

    Added best paying clients implementation and added sums to all admins

--- src/services/statistics-service.js
+++ src/services/statistics-service.js
@@ -12,20 +12,22 @@ class StatisticsService {
         startDate,
         endDate,
     }) => {
-        const bestPaidContractors = `\
-SELECT firstName || ' ' || lastName as fullName \
-FROM PROFILES \
-WHERE id IN (
-    SELECT c.contractorId as total_earnings FROM jobs j \
+        const selectBestPaidContractorsQuery = `\
+SELECT firstName || ' ' || lastName as fullName, earnings \
+FROM profiles \
+JOIN (
+    SELECT c.contractorId as id, sum(j.price) as earnings \
+    FROM jobs j \
     LEFT JOIN contracts c \
-    ON  j.contractId = c.id \
+    ON j.contractId = c.id \
     WHERE j.paymentDate BETWEEN :startDate AND :endDate \
     GROUP BY c.contractorId \
-    ORDER BY sum(j.price) DESC \
-)`;
+    ORDER BY earnings DESC \
+) contractors \
+ON contractors.id = profiles.id`;
         
         return this.sequelize.query(
-            bestPaidContractors, {
+            selectBestPaidContractorsQuery, {
                 type: QueryTypes.SELECT,
                 raw: true,
                 replacements: {
@@ -41,12 +43,32 @@ WHERE id IN (
         endDate,
         limit,
     }) => {
-        // get all jobs where paymentDate fits the dates
-        // join with contracts
-        // group by clientId
-        // sum jobs prices and sort descending
-        // return clientId
-        // use as entry to select from profiles where id in returned clientId
+        const selectBestPayingClientsQuery = `\
+SELECT firstName || ' ' || lastName as fullName, payments \
+FROM profiles \
+JOIN (
+    SELECT c.clientId as id, sum(j.price) as payments \
+    FROM jobs j \
+    LEFT JOIN contracts c \
+    ON j.contractId = c.id \
+    WHERE j.paymentDate BETWEEN :startDate AND :endDate \
+    GROUP BY c.clientId \
+    ORDER BY payments DESC \
+) clients \
+ON clients.id = profiles.id \
+LIMIT :limit`;
+        
+        return this.sequelize.query(
+            selectBestPayingClientsQuery, {
+                type: QueryTypes.SELECT,
+                raw: true,
+                replacements: {
+                    startDate,
+                    endDate,
+                    limit,
+                },
+            }
+        );
     }
 }
 
